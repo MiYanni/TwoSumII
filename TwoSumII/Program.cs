6c645ad0e150e4c3715775f253525ac95624b8ee
Michael Yanni <miyanni@microsoft.com>
2022-02-05 18:51:05 -0800

    Added VideoSolution and my own MidpointSolution.

--- TwoSumII/Program.cs
+++ TwoSumII/Program.cs
@@ -1,45 +1,62 @@
-﻿var uintInputArray = new uint[] { 1, 3, 4, 5, 7, 10, 11 };
-uint uintTarget = 9;
-var expected = (3, 4);
+﻿//var uintInputArray = new uint[] { 1, 3, 4, 5, 7, 10, 11 };
+//uint uintTarget = 9;
+//var expected = (3, 4);
 
-WriteUIntAnswer(uintInputArray, uintTarget, expected);
+//WriteUIntAnswer(uintInputArray, uintTarget, expected);
 
-uintInputArray = new uint[] { 2, 7, 11, 15 };
-uintTarget = 9;
-expected = (1, 2);
+//uintInputArray = new uint[] { 2, 7, 11, 15 };
+//uintTarget = 9;
+//expected = (1, 2);
 
-WriteUIntAnswer(uintInputArray, uintTarget, expected);
+//WriteUIntAnswer(uintInputArray, uintTarget, expected);
 
-var intInputArray = new int[] { 2, 7, 11, 15 };
-var intTarget = 9;
-expected = (1, 2);
+//var intInputArray = new int[] { 2, 7, 11, 15 };
+//var intTarget = 9;
+//expected = (1, 2);
 
-WriteIntAnswer(intInputArray, intTarget, expected);
+//WriteIntAnswer(intInputArray, intTarget, expected);
 
-intInputArray = new int[] { 2, 3, 4 };
-intTarget = 6;
-expected = (1, 3);
+//intInputArray = new int[] { 2, 3, 4 };
+//intTarget = 6;
+//expected = (1, 3);
 
-WriteIntAnswer(intInputArray, intTarget, expected);
+//WriteIntAnswer(intInputArray, intTarget, expected);
 
-intInputArray = new int[] { -1, 0 };
-intTarget = -1;
-expected = (1, 2);
+//intInputArray = new int[] { -1, 0 };
+//intTarget = -1;
+//expected = (1, 2);
 
-WriteIntAnswer(intInputArray, intTarget, expected);
+//WriteIntAnswer(intInputArray, intTarget, expected);
 
-intInputArray = new int[] { -11, -7, -5, -4, -3, -1, 0 };
-intTarget = -9;
-expected = (3, 4);
+//intInputArray = new int[] { -11, -7, -5, -4, -3, -1, 0 };
+//intTarget = -9;
+//expected = (3, 4);
 
-WriteIntAnswer(intInputArray, intTarget, expected);
+//WriteIntAnswer(intInputArray, intTarget, expected);
+
+var testCases = new (int[] inputArray, int target, (int index1, int index2) expected)[]
+{
+    (new int[] { 1, 3, 4, 5, 7, 10, 11 }, 9, (3, 4)),
+    (new int[] { 2, 7, 11, 15 }, 9, (1, 2)),
+    (new int[] { 2, 3, 4 }, 6, (1, 3)),
+    (new int[] { -1, 0 }, -1, (1, 2)),
+    (new int[] { -11, -7, -5, -4, -3, -1, 0 }, -9, (3, 4)),
+};
+
+foreach (var testCase in testCases)
+{
+    WriteAnswer(MidpointSolution, nameof(MidpointSolution),
+        testCase.inputArray, testCase.target, testCase.expected);
+}
+
+Console.ReadKey();
 
 static void WriteUIntAnswer(uint[] inputArray, uint target, (int index1, int index2) expected)
 {
     Console.WriteLine($"Name:     {nameof(UnintSolution2)}");
     Console.Write($"Input:    (");
     inputArray.Select((e, i) => (e, i)).ToList()
-        .ForEach(ei => Console.Write($"{ei.e}{(ei.i + 1 == inputArray.Length ? string.Empty : ",")}"));
+        .ForEach(ei => Console.Write($"{ei.e}{(ei.i + 1 == inputArray.Length ? string.Empty : ", ")}"));
     Console.WriteLine(")");
     Console.WriteLine($"Target:   {target}");
     Console.WriteLine($"Expected: {expected}");
@@ -52,11 +69,24 @@ static void WriteIntAnswer(int[] inputArray, int target, (int index1, int index2
     Console.WriteLine($"Name:    {nameof(IntSolution1)}");
     Console.Write($"Input:    (");
     inputArray.Select((e, i) => (e, i)).ToList()
-        .ForEach(ei => Console.Write($"{ei.e}{(ei.i + 1 == inputArray.Length ? string.Empty : ",")}"));
+        .ForEach(ei => Console.Write($"{ei.e}{(ei.i + 1 == inputArray.Length ? string.Empty : ", ")}"));
     Console.WriteLine(")");
     Console.WriteLine($"Target:   {target}");
     Console.WriteLine($"Expected: {expected}");
     Console.WriteLine($"Actual:   {IntSolution1(inputArray, target)}");
+    Console.WriteLine();
+}
+
+static void WriteAnswer(Func<int[], int, (int index1, int index2)> solution, string solutionName, int[] inputArray, int target, (int index1, int index2) expected)
+{
+    Console.WriteLine($"Name:    {solutionName}");
+    Console.Write($"Input:    (");
+    inputArray.Select((e, i) => (e, i)).ToList()
+        .ForEach(ei => Console.Write($"{ei.e}{(ei.i + 1 == inputArray.Length ? string.Empty : ", ")}"));
+    Console.WriteLine(")");
+    Console.WriteLine($"Target:   {target}");
+    Console.WriteLine($"Expected: {expected}");
+    Console.WriteLine($"Actual:   {solution(inputArray, target)}");
     Console.WriteLine();
 }
 
@@ -189,5 +219,82 @@ static (int index1, int index2) IntSolution1(int[] inputArray, int target)
         differencesWithIndex.Push((i, target - element));
     }
 
+    throw new InvalidDataException();
+}
+
+static (int index1, int index2) VideoSolution(int[] inputArray, int target)
+{
+    var leftIndex = 0;
+    var rightIndex = inputArray.Length - 1;
+
+    while (leftIndex < rightIndex)
+    {
+        var sum = inputArray[leftIndex] + inputArray[rightIndex];
+        if (sum > target)
+        {
+            rightIndex--;
+        }
+        else if (sum < target)
+        {
+            leftIndex++;
+        }
+        else
+        {
+            return (leftIndex + 1, rightIndex + 1);
+        }
+    }
+    throw new InvalidDataException();
+}
+
+static (int index1, int index2) MidpointSolution(int[] inputArray, int target)
+{
+    var leftIndex = 0;
+    var rightIndex = inputArray.Length - 1;
+    var midIndex = inputArray.Length / 2;
+
+    while (leftIndex < rightIndex)
+    {
+        var sum = inputArray[leftIndex] + inputArray[rightIndex];
+        if (sum > target)
+        {
+            rightIndex--;
+            var midSum = inputArray[leftIndex] + inputArray[midIndex];
+            if(midSum > target)
+            {
+                leftIndex = midIndex - 1;
+                midIndex = (leftIndex + rightIndex) / 2;
+                continue;
+            }
+            if (midSum < target)
+            {
+                rightIndex = midIndex + 1;
+                midIndex = (leftIndex + rightIndex) / 2;
+                continue;
+            }
+
+            return (leftIndex + 1, midIndex + 1);
+        }
+        if (sum < target)
+        {
+            leftIndex++;
+            var midSum = inputArray[midIndex] + inputArray[rightIndex];
+            if (midSum > target)
+            {
+                leftIndex = midIndex - 1;
+                midIndex = (leftIndex + rightIndex) / 2;
+                continue;
+            }
+            if (midSum < target)
+            {
+                rightIndex = midIndex + 1;
+                midIndex = (leftIndex + rightIndex) / 2;
+                continue;
+            }
+
+            return (midIndex + 1, rightIndex + 1);
+        }
+
+        return (leftIndex + 1, rightIndex + 1);
+    }
     throw new InvalidDataException();
 }
